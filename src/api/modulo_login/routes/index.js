c96b10ec3e834d00cb11399af78abf4fa0450af0
Jeral <jeral.cris@gmail.com>
2021-07-22 16:14:56 -0600

    cambiando el tamaño minimo de las contraseñas

--- src/api/modulo_login/routes/index.js
+++ src/api/modulo_login/routes/index.js
@@ -13,7 +13,7 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { esAdminRole, tieneRol } = require('../middlewares/validar-rol');
 
 // Rutas API modulo_login
-router.post('/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], login);
+router.post('/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 6 }), validarCampos], login);
 
 // Ruta recuperar contraseña
 router.post('/recuperarContrasenia', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), validarCampos], recuperarPassword);
@@ -22,7 +22,7 @@ router.post('/recuperarContrasenia', [check('correo', 'Debe ser un correo valido
 router.post('/recuperarPorPreguntas', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('respuesta', 'La respuesta es obligatoria').not().isEmpty(), validarCampos], passwordPreguntas);
 
 // Ruta para crear la nueva contraseña
-router.put('/nuevaContrasenia', [check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], newPassword);
+router.put('/nuevaContrasenia', [check('password', 'La contraseña debe contener al menos 6 dígitos y es requerido').isLength({ min: 6 }), validarCampos], newPassword);
 
 // Ruta para probar los middlewares
 router.get('/usuarios/:id', [validarJWT, esAdminRole, tieneRol(1, 3)], user);
